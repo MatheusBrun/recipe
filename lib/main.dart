85d6074e407fef18d9a6646069a9bb1b2b7359e2
Matheus_bruno <mte.bruno1@gmail.com>
2022-06-26 22:19:01 -0300

    fonts, category_item, categories screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:recipe/screens/categories_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  final ThemeData theme = ThemeData();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Vamos Cozinhar?',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({ Key? key }) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Vamos Cozinhar'),
-      ),
-      body: Center(
-        child: Text('Navegar é importante!!'),
-      ),
+      theme: theme.copyWith(
+          colorScheme: theme.colorScheme.copyWith(
+            primary: Colors.blue,
+            secondary: Color.fromARGB(255, 18, 80, 131),
+          ),
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          textTheme: theme.textTheme.copyWith(
+            headline6: TextStyle(
+                fontFamily: 'RobotoCondensed',
+                fontSize: 16,
+                color: Colors.black),
+          )),
+      home: CategoriesScreen(),
     );
   }
 }
--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:recipe/components/category_item.dart';
+import 'package:recipe/data/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({ Key? key }) : super(key: key);
+  const CategoriesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 /2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
-
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vamos Cozinhar?'),
       ),
-      children: [
-        
-      ],
+      body: GridView(
+        padding: EdgeInsets.all(25),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES.map((cat) {
+          return CategoryItem(cat);
+        }).toList(),
+      ),
     );
   }
 }
